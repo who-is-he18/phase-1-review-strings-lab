c1c0e22af2cb430129738ea211c5f8ec23ab7a31
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-06-24 15:37:24 +0300

    lab.md

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code in this file!
+// Write your code here//
+const currentUser = 'Geoffrey Nganga';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+
+
+
